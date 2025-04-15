31de99cd9fbdd5c3bfeebdeb3611bbfe984eeb7b
Lucas Carvalho De Jesus Oliveira <lucascarvalhoptc06@gmail.com>
2025-04-15 15:47:32 -0300

    Add files via upload

---
+++ operação.js
@@ -0,0 +1,19 @@
+let a = Number (prompt ("Qual e o numero: ") );
+let b = Number (prompt ("Qual e o proximo numero: ") );
+
+
+
+let soma = a + b; 
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+
+console.log ("Soma " + soma);
+console.log ("Subtração " + subtracao);
+console.log ("Multiplicação " + multiplicacao);
+console.log ("Divisão " + divisao);
+
+alert ("Soma " + soma);
+alert ("Subtração " + subtracao);
+alert ("Multiplicação " + multiplicacao);
+alert ("Divisão " + divisao);
