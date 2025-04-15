0863d4548d6f16589a404c568acbd72f3ba42280
Lucas Carvalho De Jesus Oliveira <lucascarvalhoptc06@gmail.com>
2025-04-15 20:01:14 -0300

    Update Exercício 1.js

--- Exercício 1.js
+++ Exercício 1.js
@@ -1,7 +1,7 @@
     function calcularamedia() {
         const numero1 = Number (prompt ("Qual é o primeiro valor: "));
         const numero2 = Number (prompt ("Qual é o segundo valor: "));
-        const numero3 = Number (prompt ("Qaul é o terçeiro numero "));
+        const numero3 = Number (prompt ("Qual é o terceiro valor "));
 
         const media = (numero1 + numero2 + numero3) / 3;
 
